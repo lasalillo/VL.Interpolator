9b4fcf0480a86ae421796613be57e60932ed81d7
Aristides Garcia <aristides_job@hotmail.com>
2024-03-14 17:17:04 +0100

    Added MultiBlend<T>

--- src/Enums/InterpolatorEnums.cs
+++ src/Enums/InterpolatorEnums.cs
@@ -3,6 +3,8 @@
     public enum InterpolationModes { Interpolate, Switch, Minimum, Maximum };
     public enum InterpolationModes2 { Interpolate, Switch };
     public enum SwitchModes { Switch, Minimum, Maximum };
+    public enum BlendModes { Direct, Add, Multiply, Minimum, Maximum };
+    public enum BooleanModes { Direct, OR, AND, NOR, NAND, XOR, XNOR };
     public enum TweenerTransition2 { Linear, Sine, Quad, Cubic, Quart, Quint, Expo, Circular, Elastic, Back, Switch };
     public enum TweenerTransitionFade { Linear, Sine, Quad, Cubic, Quart, Quint, Expo, Circular };
 }
